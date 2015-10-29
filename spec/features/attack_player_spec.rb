3ccb75280540f39da0758706e86086629dc089ec
mongolianprincess <ayc.yang@gmail.com>
2015-10-29 16:37:06 +0000

    add feature test to HP - ch.24

--- spec/features/attack_player_spec.rb
+++ spec/features/attack_player_spec.rb
@@ -6,4 +6,11 @@ feature 'A player attacks' do
     click_button('Attack')
     expect(page).to have_content('Rebel Alliance attacked The Empire')
   end
+
+  scenario 'player 2 hp deducted by 10' do
+    sign_in_and_play
+    click_button('Attack')
+    click_button('Continue battle')
+    expect(page).to have_content('The Empire 90/100HP')
+  end
 end
