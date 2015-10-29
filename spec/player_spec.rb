f194c459917306971b0107eb0d2c4d6bb634a5fa
samover <samuel.overloop@gmail.com>
2015-10-29 16:46:37 +0000

    added unit tests for hitpoints

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,11 +1,23 @@
 require 'player'
 
 describe Player do
-  subject(:player) { described_class.new(:name) }
-
+  subject(:player_one) { described_class.new('Luke Skywalker') }
+  subject(:player_two) { described_class.new('Darth Vader') }
   describe '#name' do
     it 'returns a player\'s name' do
-      expect(player.name).to eq :name
+      expect(player_one.name).to eq 'Luke Skywalker'
+    end
+  end
+  
+  describe '#hitpoints' do
+    it 'shows a player hitpoints' do
+      expect(player_one.hitpoints).to eq Player::DEFAULT_HP
+    end
+  end
+
+  describe '#attack' do
+    it 'player one attacks player two' do
+      expect{player_one.attack player_two}.to change{player_two.hitpoints}.by(-10)
     end
   end
 end
