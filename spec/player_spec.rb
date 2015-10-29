5f4a0018e6b54f047f72db2603a4cae1933084b9
samover <samuel.overloop@gmail.com>
2015-10-29 15:49:16 +0000

    added feature test for player class

---
+++ spec/player_spec.rb
@@ -0,0 +1,11 @@
+require 'player'
+
+describe Player do
+  subject(:player) { described_class.new(:name) }
+  
+  describe '#name' do
+    it 'returns a player\'s name' do
+      expect(player.name).to eq :name
+    end
+  end
+end
