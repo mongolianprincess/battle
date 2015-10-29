1ff81a24a0c3a60e1e04f28233c098e46e5bce21
mongolianprincess <ayc.yang@gmail.com>
2015-10-29 15:38:41 +0000

    ch.22

--- app.rb
+++ app.rb
@@ -12,11 +12,17 @@ class Battle < Sinatra::Base
     session[:name2] = params[:name2]
     redirect '/play'
   end
-  
+
   get '/play' do
     @player1_name = session[:name1]
     @player2_name = session[:name2]
     erb :play
+  end
+
+  get '/attack' do
+    @player1_name = session[:name1]
+    @player2_name = session[:name2]
+    erb :attack
   end
 
   # start the server if ruby file executed directly
